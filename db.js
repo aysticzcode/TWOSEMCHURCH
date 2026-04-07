b5bd189c9e9eeb9f38bc3d1375a4ee6e65f6556d
aysticzcode <jonessmithgraphics@gmail.com>
2026-04-07 07:44:04 -0700

    Add files via upload

---
+++ db.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAbH1BkQf_uGrObUgs75FnGP6jyXVvx-UY",
+  authDomain: "my-church-web-4d268.firebaseapp.com",
+  projectId: "my-church-web-4d268",
+  storageBucket: "my-church-web-4d268.firebasestorage.app",
+  messagingSenderId: "332320473114",
+  appId: "1:332320473114:web:16312733d0d6c6815451a3"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
